69aebbc7ba34d575e456d16694563aca52a7f9e1
Luciano Ramalho <luciano@ramalho.org>
2021-03-23 15:48:25 -0300

    mymax: final step, 6 overloads

---
+++ less_than/max_steps/99-final/mymax.py
@@ -0,0 +1,54 @@
+from typing import Protocol, Any, TypeVar, overload, Callable, Iterable, Union
+
+class SupportsLessThan(Protocol):
+    def __lt__(self, other: Any) -> bool: ...
+
+T = TypeVar('T')
+LT = TypeVar('LT', bound=SupportsLessThan)
+DT = TypeVar('DT')
+
+MISSING = object()
+EMPTY_MSG = 'max() arg is an empty sequence'
+
+@overload
+def max(arg1: LT, arg2: LT, *rest: LT, key: None = ...) -> LT:
+    ...
+@overload
+def max(arg1: T, arg2: T, *rest: T, key: Callable[[T], LT]) -> T:
+    ...
+@overload
+def max(iterable: Iterable[LT], *, key: None = ...) -> LT:
+    ...
+@overload
+def max(iterable: Iterable[T], *, key: Callable[[T], LT]) -> T:
+    ...
+@overload
+def max(iterable: Iterable[LT], *, key: None = ..., default: DT) -> Union[LT, DT]:
+    ...
+@overload
+def max(iterable: Iterable[T], *, key: Callable[[T], LT], default: DT) -> Union[T, DT]:
+    ...
+def max(first, *rest, key=None, default=MISSING):
+    if rest:
+        series = rest
+        candidate = first
+    else:
+        series = iter(first)
+        try:
+            candidate = next(series)
+        except StopIteration:
+            if default is not MISSING:
+                return default
+            raise ValueError(EMPTY_MSG) from None
+    if key is None:
+        for current in series:
+            if candidate < current:
+                candidate = current
+    else:
+        candidate_key = key(candidate)
+        for current in series:
+            current_key = key(current)
+            if candidate_key < current_key:
+                candidate = current
+                candidate_key = current_key
+    return candidate
