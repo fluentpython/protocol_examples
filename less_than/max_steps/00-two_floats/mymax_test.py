bd476fec64549d0d079e6be5394b899f23a551ef
Luciano Ramalho <luciano@ramalho.org>
2021-05-09 20:10:45 -0300

    mymax: step 00

--- less_than/max_steps/00-two_floats/mymax_test.py
+++ less_than/max_steps/00-two_floats/mymax_test.py
@@ -5,11 +5,11 @@ import pytest
 @pytest.mark.parametrize(
     'a, b, expected',
     [
-        (1, 2, 2),
-        (-3, -1, -1),
-        (4, 4, 4),
+        (1.1, 2.2, 2.2),
+        (-3.1, -1.3, -1.3),
+        (4.2, 4.2, 4.2),
     ],
 )
-def test_two_ints(a: float, b: float, expected: float) -> None:
+def test_two_floats(a: float, b: float, expected: float) -> None:
     result: float = mymax.max(a, b)
     assert result == expected
